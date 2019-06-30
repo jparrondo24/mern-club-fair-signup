231717667f497eb20d911c861ab23eb32959a9e9
Justin Parrondo <justinparrondo@gmail.com>
2019-06-29 21:17:13 -0400

    Configures dotenv and mongoose for MongoDB database connection to mLab

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const os = require('os');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', () => {
+  console.log("Successfully connected to MongoDB database");
+});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
